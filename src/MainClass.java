b02435df05c63debeece16165e7967399283ca74
Svyatoslav Lyakh <svyatoslavlyakh@MacBook-Air-Svyatoslav.local>
2021-06-11 02:16:02 +0600

    Add HW_1.2 and HW_1.3

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,32 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 public class MainClass
 {
+// Задание 1
     public int getLocalNumber ()
     {
         int number = 15;
         return number;
+    }
+
+
+// Задание 2
+    public int getClassNumber ()
+    {
+        return class_number;
+    }
+    private int class_number;
+
+    {
+        class_number = 20;
+    }
+
+
+// Заданиее 3
+    private String class_string = "Hello world";
+
+    public String getClassString()
+    {
+        return class_string;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,7 +1,9 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest extends MainClass
 {
+// Задание 1
     @Test
     public void testGetLocalNumber ()
     {
@@ -11,5 +13,27 @@ public class MainClassTest extends MainClass
         } else {
             System.out.println("Number != 14");
         }
+    }
+
+// Задание 2
+    @Test
+    public void testGetClassNumber ()
+    {
+        int b = this.getClassNumber();
+        if (b > 45) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Number < 45");
+        }
+
+    }
+
+// Задание 3
+    @Test
+    public void testGetClassString()
+    {
+        String c = this.getClassString();
+        Assert.assertTrue("Подстрока 'hello'/'Hello' не входит в строку",(c.contains("Hello")) | (c.contains("hello")));
+
     }
 }
